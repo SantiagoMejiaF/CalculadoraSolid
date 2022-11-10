0b07484ed31e17d3529cec07c0e3eb167d82b91f
Luis Bravo <luisalejandrobravoferreira@gmail.com>
2022-11-09 20:13:28 -0500

    Division Funcional - Luis Bravo

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,10 +7,6 @@ import org.example.menu.MenuCalculadora;
 
 public class App {
 
-
-
-
-
     public static void main(String[] args)  {
         MenuCalculadora menuCalculadora = new MenuCalculadora();
         menuCalculadora.imprimirMenu();
--- src/main/java/org/example/operaciones/OperacionDivision.java
+++ src/main/java/org/example/operaciones/OperacionDivision.java
@@ -8,6 +8,7 @@ public class OperacionDivision implements OperacionBinaria{
 
     private BigDecimal dividir(BigInteger num1, BigInteger num2)
     {
+        // Implementacion de la division con programacion funcional.
         BiFunction<BigInteger, BigInteger, BigDecimal> dividir = (n1, n2) ->
                 new BigDecimal(n1.divide(n2));
         return dividir.apply(num1, num2);
